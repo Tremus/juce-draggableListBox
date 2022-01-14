1c8a90a8eca45b15161fee25b8c6c22a740137bc
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2022-01-14 14:53:49 +1100

    Constrained item vertically, and explained optional behaviour in comments

--- Source/MyListComponent.h
+++ Source/MyListComponent.h
@@ -13,7 +13,7 @@ public:
     int size() override { return rowIds.size(); }
     void swapRows(int sourceRowidx, int targetRowIdx) override { rowIds.swap(sourceRowidx, targetRowIdx); }
 
-    void paintRow(DraggableListBoxItem* item, int rowNumber, Graphics& g, Rectangle<int> bounds) override
+    void paintRow(DraggableListBoxItem*, int rowNumber, Graphics& g, Rectangle<int> bounds) override
     {
         // If row is being dragged, paint opaque image
         if (dragRowIdx == rowNumber)
@@ -28,8 +28,14 @@ public:
         g.drawText("Item: " + juce::String(rowIds[rowNumber]), bounds, Justification::centred);
     }
 
+    // OPTIONAL. See item 5. in the README
     void deleteRow(int idx) override { rowIds.remove(idx); };
-    void addItemAtEnd() { rowIds.add(idCounter); idCounter++; };
+
+    void addItemAtEnd()
+    {
+        rowIds.add(idCounter);
+        idCounter++;
+    };
 
     // Not required, just something I'm adding for confirmation of correct order after DnD.
     // This is an example of an operation on the entire list.
@@ -46,6 +52,7 @@ public:
     juce::Array<int> rowIds;
 
 private:
+    // All ids are unique
     int idCounter = 0;
 };
 
@@ -58,10 +65,20 @@ class MyListBox : public DraggableListBox
 public:
     MyListBox(MyListBoxItemData& data) : DraggableListBox(data) {}
 
+    // OPTIONAL. See item 6. in the README
     void dragImageMove(juce::Point<int>& defaultTopLeft) override
     {
+        // Constrains the image horizontally
         defaultTopLeft.x = 0;
+        
+        // Constrains the image vertically when user drags row up
         if (defaultTopLeft.y < 0)
             defaultTopLeft.y = 0;
+
+        int rowHeight = getRowHeight();
+        int bottom = defaultTopLeft.y + rowHeight;
+        // Constrains the image vertically when user drags row down
+        if (bottom > getHeight())
+            defaultTopLeft.y = getHeight() - rowHeight;
     }
 };
